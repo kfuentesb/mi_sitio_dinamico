de9495dc1dfd9c2336cd0d0ff35a5665b7bc8c3e
kfuentesb <kevfueben@gmail.com>
2025-10-06 10:28:57 +0200

    Añadiendo jamón ibérico a los productos

--- elementos/contenido.php
+++ elementos/contenido.php
@@ -3,7 +3,8 @@
 $productos = [
   "Pan de Camas"        => 1.20,
   "Aceitunas aliñadas"  => 2.50,
-  "Tortas de aceite"    => 3.00
+  "Tortas de aceite"    => 3.00,
+  "Jamón ibérico"     => 15.00
 ];
 ?>
 
